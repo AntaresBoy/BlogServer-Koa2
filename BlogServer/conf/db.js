306ca698950c689bd2fcdab4807a03a72b54f8db
AntaresBoy <2531646835@qq.com>
2022-02-10 23:31:41 +0800

    11

--- BlogServer/conf/db.js
+++ BlogServer/conf/db.js
@@ -1,18 +1,30 @@
 const env = process.env.NODE_ENV
 let MYSQL_CONF
 let REDIS_CONF
+// const DB_NAME = {
+//   users: 'ids_users',
+//   blogs: 'ids_blogs'
+// }
+
 const DB_NAME = {
-  users: 'ids_users',
-  blogs: 'ids_blogs'
+  users: 'users',
+  blogs: 'blog'
 }
 //开发环境数据库信息
 if (env === 'dev') {
+  // MYSQL_CONF = {
+  //   host: "10.7.235.231",
+  //   user: "root",
+  //   password: "Ids@132132",
+  //   port: 3305,
+  //   database: 'ids_test'
+  // }
   MYSQL_CONF = {
-    host: "10.7.235.231",
+    host: "39.108.119.239",
     user: "root",
-    password: "Ids@132132",
-    port: 3305,
-    database: 'ids_test'
+    password: "lpq",
+    port: 3306,
+    database: 'MY_BLOG'
   }
   REDIS_CONF = {
     port: 6379,
@@ -22,12 +34,19 @@ if (env === 'dev') {
 
 //线上数据库链接信息
 if (env === 'production') {
+  // MYSQL_CONF = {
+  //   host: "10.7.235.231",
+  //   user: "root",
+  //   password: "Ids@132132",
+  //   port: 3305,
+  //   database: 'ids_test'
+  // }
   MYSQL_CONF = {
-    host: "10.7.235.231",
+    host: "39.108.119.239",
     user: "root",
-    password: "Ids@132132",
-    port: 3305,
-    database: 'ids_test'
+    password: "lpq",
+    port: 3306,
+    database: 'MY_BLOG'
   }
 
   REDIS_CONF = {
