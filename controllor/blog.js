1008c356f753fd7f65bc76de1f0be89ca896d607
AntaresBoy <2531646835@qq.com>
2022-03-17 19:41:17 +0800

    feat:新增文章发表概述

--- controllor/blog.js
+++ controllor/blog.js
@@ -93,7 +93,7 @@ const deleteBlog = async (contentId, author) => {
 }
 
 const searchBlogs = async (keyword) => {
-  let sql = `select id,title,createtime,author,tags,contentId,content from ${DB_NAME.blogs} where 1=1 and isDeleted=0 `
+  let sql = `select id,title,createtime,author,tags,contentId,content,overview from ${DB_NAME.blogs} where 1=1 and isDeleted=0 `
   sql += `and (title like "%${keyword}%" or content like "%${keyword}%" or author like "%${keyword}%") order by createtime desc;`
   if (!keyword) return getAllBlogs();
   console.log("searchBlogs:", sql, keyword);
